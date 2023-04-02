a6d225ead567907d7ebecac5b55d87896d9e7f5b
namduong2006 <129411334+namduong2006@users.noreply.github.com>
2023-04-02 15:52:43 +0700

    Add project files.

---
+++ TinhSchunhat/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinhSchunhat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            float dai, rong, s;
+            Console.Write("Nhập chiều dài hình chữ nhật:");
+            dai= float.Parse(Console.ReadLine());
+            Console.Write("Mời nhập chiều rộng hình chữ nhật:");
+            rong= float.Parse(Console.ReadLine());
+            s = dai * rong;
+            Console.WriteLine("Diện tích hình chữ nhật là: " +s);
+            Console.ReadLine();
+        }
+    }
+}
